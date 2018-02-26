23be329a175f8dc09143948f6714863b43cc6ce0
ManishaMuskan <manigetintouch@gmail.com>
2018-02-26 09:25:44 +0530

    Error page added

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,11 @@
-<h1>Unauthorised access!!</h1>
+@extends('templates.default')
+
+@section('title')
+  chatty : a social app
+@endsection
+
+@section('content')
+  <div class="container">
+        <h1 class="text-center">Unauthorised access!!</h1>
+  </div>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,11 @@
-<h1>Not Found</h1>
+@extends('templates.default')
+
+@section('title')
+  chatty : a social app
+@endsection
+
+@section('content')
+  <div class="container">
+        <h1 class="text-center">Not Found</h1>
+  </div>
+@endsection

e9f239ee949d141017b4187f9c02283af3c26365
ManishaMuskan <manigetintouch@gmail.com>
2018-03-02 06:36:05 +0530

    Friendship feature added

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,9 @@ namespace chatty\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class User extends Authenticatable
 {
-
     protected $fillable = [
         'username',
         'email',
@@ -15,15 +15,15 @@ class User extends Authenticatable
         'last_name',
         'location',
     ];
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Set attributes to save in a particular format in db
-     *
+     /*
+     |--------------------------------------------------------------------------
+     | Set attributes to save in a particular format in db
+     |--------------------------------------------------------------------------
      */
     public function setPasswordAttribute($password)
     {
@@ -40,10 +40,22 @@ class User extends Authenticatable
       $this->attributes['last_name'] = ucfirst ($last_name);
     }
 
-    /**
-     * The Functions to get results from db
-     *
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Get Gravatar URL
+    |--------------------------------------------------------------------------
+    */
+     public function getGravatarImg($size){
+       return 'https://www.gravatar.com/avatar/{{ md5(strtolower(trim($this->email))) }}?d=mm&s='.$size;
+     }
+
+    /*
+    |--------------------------------------------------------------------------
+    | The Functions to get results from db
+    |--------------------------------------------------------------------------
+    */
+
+    /*firstName or fullname or username*/
     public function getName()
     {
       if ($this->first_name && $this->last_name) {
@@ -65,11 +77,76 @@ class User extends Authenticatable
       return $this->first_name ?: $this->username;
     }
 
-    /**
-     * Get Gravatar URL
-     *
-     */
-     public function getGravatarImg($size){
-       return 'https://www.gravatar.com/avatar/{{ md5(strtolower(trim($this->email))) }}?d=mm&s='.$size;
-     }
+    /*User to another user relationship - friends*/
+    // Relation - friendship that I started
+    public function friendsOfMine(){
+      return $this->belongstoMany('chatty\Models\User', 'friends', 'user_id', 'friend_id')->withPivot('accepted')->withTimestamps(); //By using user_id, pick the row from friends table and then search for the user (having corresponding friend-id) from the user table.
+    }
+
+    // Relation - friendship that I was invited to
+    public function iAmFriendOf(){
+      return $this->belongstoMany('chatty\Models\User', 'friends', 'friend_id', 'user_id')->withPivot('accepted')->withTimestamps(); //By using friend_id, pick the row from friends table and then search for the user (having corresponding user-id) from the user table.
+    }
+
+    // friends after accepting the rquest
+    public function friends(){
+      return $this->friendsOfMine()->wherePivot('accepted', true)->get()
+             ->merge($this->iAmFriendOf()->wherePivot('accepted', true)->get());
+    }
+
+    //friends which i have not accepted
+    public function friendRequests(){
+      return $this->iAmFriendOf()->wherepivot('accepted', false)->get();
+    }
+
+    //get friends i added but am not accepted
+    public function sentRequests(){
+      return $this->friendsOfMine()->wherepivot('accepted', false)->get();
+    }
+
+    //check if a friend i added has accepted my request
+      public function ifsentRequestPending(User $receipent){
+      return (bool) $this->sentRequests()->where('id', $receipent -> id)->count();
+    }
+
+    //check if a friend i added has accepted my request
+    public function ifReceivedRequestPending(User $sender){
+      return (bool) $this->friendRequests()->where('id', $sender -> id)->count();
+    }
+
+    //check if both are friends
+    public function ifIsFriendWith(User $user){
+      return (bool) $this->friends()->where('id', $user -> id)->count();
+    }
+
+    //add friend by attaching id to the intermediate table
+    public function addAsMyFriend(User $user){
+      return $this->friendsOfMine()->attach($user-> id);
+    }
+
+    //add friend by attaching id to the intermediate table
+    public function acceptFriendRequest(User $sender){
+      return $this->friendRequests()->where('id', $sender -> id)->first()->pivot
+             ->update([
+               'accepted' => true,
+             ]);
+    }
+
+    //add friend by attaching id to the intermediate table
+    public function ignoreFriendRequest(User $sender){
+      return $this->iAmFriendOf()->detach($sender-> id);
+    }
+
+    //detaching user - unfriend funcionality
+    public function removeAsFriend(User $friend)
+    {
+      /*check if user sent request*/
+      if($this->iAmFriendOf()->wherepivot('accepted', true)->where('id', $friend -> id)->count()){
+        return $this->iAmFriendOf()->detach($friend-> id);
+      }
+      /*check if i sent request*/
+      if($this->friendsOfMine()->wherepivot('accepted', true)->where('id', $friend -> id)->count()){
+        return $this->friendsOfMine()->detach($friend-> id);
+      }
+    }
 }
